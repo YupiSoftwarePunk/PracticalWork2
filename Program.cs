57984eec46d68df0bd3a053f9d4ccb675ddf1155
Yupi <youngperchik2404@gmail.com>
2025-04-13 14:20:38 +0500

    Adited menu

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@
             while (true)
             {
                 Console.WriteLine("Меню магазина\n");
+                Console.WriteLine("1. Загрузить данные");
+                Console.WriteLine("2. Создать отчет");
+                Console.WriteLine("3. Найти проклятые артефакты");
+                Console.WriteLine("4. Группировка артефактов по редкости");
+                Console.WriteLine("5. Топ артефактов по силе");
+                Console.WriteLine("0. Выход");
+
 
             }
         }
